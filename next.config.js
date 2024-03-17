5857bdbb370abc495a9b05ef55e867bb8616d774
Kunal Hulke <hulke1102002@gmail.com>
2024-03-17 16:47:32 +0530

    resolving the production errors

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: [
-      'res.cloudinary.com', 
-      'avatars.githubusercontent.com',
-      'lh3.googleusercontent.com'
-    ]
-  }
-}
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+      "res.cloudinary.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+export default nextConfig;
